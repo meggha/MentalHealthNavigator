eeb8a1c91143dadc3a6ddc17c65699c1c002abf5
sushant607 <svbagul_b22@ce.vjti.ac.in>
2024-03-02 19:22:30 +0530

    Update ActionProvider.jsx

--- client/src/bot/ActionProvider.jsx
+++ client/src/bot/ActionProvider.jsx
@@ -9,9 +9,6 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
     setIsLoading(true);
     setError(null);
     try {
-=======
-      const response = await axios.post('https://e680-35-194-181-27.ngrok-free.app', { message });
->>>>>>> fd47331a4b14eebb31cb7ff8336aa47666cbfb50
       const response = await axios.post('https://b6fd-34-125-153-14.ngrok-free.app', { message });
   
       if (response.status !== 200) {
